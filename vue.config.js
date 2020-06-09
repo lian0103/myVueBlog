f90f159e8911f835a5400ffa7a4ef69d3e53439a
lian0103 <k1319900103@gmail.com>
2020-06-10 00:26:29 +0800

    sh 打包方式

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // const path = require('path')  
 module.exports = {  
- publicPath: './', 
+ publicPath: process.env.NODE_ENV === 'production' ? '/vueBlog2019/'
+ : './', 
  // 預設為/ 所有html下的js css 連結都會是/js/xx,在輸出到web server 路徑會有問題,需用./  
 } 
